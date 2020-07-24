ae37eab4645a033255bb577ac337e89522f4d005
seancabahug <25161923+seancabahug@users.noreply.github.com>
2020-07-24 14:36:53 -0700

    built frontend

---
+++ frontend/build/precache-manifest.99bed40793c984c2e7f4c4cfb88f277d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2026ceff3b9b86a46052b397981293da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c12041c031c3946d5561",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "f6f84b366ad4652849ae",
+    "url": "/static/css/main.92a567d6.chunk.css"
+  },
+  {
+    "revision": "c12041c031c3946d5561",
+    "url": "/static/js/2.8963557e.chunk.js"
+  },
+  {
+    "revision": "5b5988d9d94b401ac519cbfa45aa6fa9",
+    "url": "/static/js/2.8963557e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6f84b366ad4652849ae",
+    "url": "/static/js/main.657b5391.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
